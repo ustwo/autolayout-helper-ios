404f7e237e06ed1e529d321af9a1a1966091a846
Shagun Madhikarmi <shagun@ustwo.com>
2015-06-15 20:41:06 +0100

    Added interface for public methods

---
+++ AutoLayoutHelper/AutoLayoutHelper/UIView+AutoLayoutHelper.h
@@ -0,0 +1,81 @@
+//
+//  UIView+AutoLayoutHelper.h
+//  AutoLayoutHelper
+//
+//  Created by Shagun Madhikarmi on 15/06/2015.
+//  Copyright (c) 2015 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (AutoLayoutHelper)
+
+
+#pragma mark - Constraints
+
+
+#pragma mark-Fill
+
+- (NSArray *)fillSuperView:(UIEdgeInsets)edges;
+
+
+#pragma mark-Left
+
+- (NSLayoutConstraint *)addLeftConstraintToView:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addLeftConstraintToView:(UIView *)toView attribute:(NSLayoutAttribute)toAttribute relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-Right
+
+- (NSLayoutConstraint *)addRightConstraintToView:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addRightConstraintToView:(UIView *)toView attribute:(NSLayoutAttribute)toAttribute relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-Top
+
+- (NSLayoutConstraint *)addTopConstraintToView:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addTopConstraintToView:(UIView *)toView attribute:(NSLayoutAttribute)toAttribute relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-Bottom
+
+- (NSLayoutConstraint *)addBottomConstraintToView:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addBottomConstraintToView:(UIView *)toView attribute:(NSLayoutAttribute)toAttribute relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-CenterX
+
+- (NSLayoutConstraint *)addCenterXConstraintToView:(UIView *)toView;
+
+- (NSLayoutConstraint *)addCenterXConstraintToView:(UIView *)toView constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addCenterXConstraintToView:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-CenterY
+
+- (NSLayoutConstraint *)addCenterYConstraintToView:(UIView *)toView;
+
+- (NSLayoutConstraint *)addCenterYConstraintToView:(UIView *)toView constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addCenterYConstraintToView:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-Width
+
+- (NSLayoutConstraint *)addWidthConstraint:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addWidthConstraint:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+
+#pragma mark-Height
+
+- (NSLayoutConstraint *)addHeightConstraint:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+- (NSLayoutConstraint *)addHeightConstraint:(UIView *)toView relation:(NSLayoutRelation)relation constant:(CGFloat)constant;
+
+@end
